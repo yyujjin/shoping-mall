fc92242db39f525e96c5b43aa49459c4f5c7e8af
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-08-30 00:15:56 +0900

    get 라우터 만듦.

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+require('dotenv').config();
+//npm install mysql 터미널에 mysql연결해주는 명령어 
+
+app.get("/cart", (req, res) => {
+    var mysql = require("mysql")
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: "study",
+    })
+
+    connection.connect()
+
+    connection.query(
+        "select * from cart",
+        function (err, rows, fields) {
+            if (err) throw err
+            console.log(rows)
+
+            connection.end()
+            res.send(rows)
+        }
+    )
 })
 
 app.listen(port, () => {
